ec1e3873a8862593cbf7b11da17f331f0298f012
wgsainzl <wgsainzl@gmail.com>
2024-11-26 09:26:37 -0600

    changes in main

---
+++ cmake-build-debug/CMakeFiles/Inventario.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Inventario.dir/Inventory.cpp.obj"
+  "CMakeFiles/Inventario.dir/Inventory.cpp.obj.d"
+  "CMakeFiles/Inventario.dir/main.cpp.obj"
+  "CMakeFiles/Inventario.dir/main.cpp.obj.d"
+  "Inventario.exe"
+  "Inventario.exe.manifest"
+  "Inventario.pdb"
+  "libInventario.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Inventario.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
