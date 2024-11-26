4251c5ec10dd2d6a42a5304690e5709df85ff98c
wgsainzl <wgsainzl@gmail.com>
2024-11-26 09:22:18 -0600

    Add history

---
+++ History.cpp
@@ -0,0 +1,5 @@
+//
+// Created by willy on 26/11/2024.
+//
+
+#include "History.h"
---
+++ History.h
@@ -0,0 +1,16 @@
+//
+// Created by willy on 26/11/2024.
+//
+
+#ifndef HISTORY_H
+#define HISTORY_H
+
+
+
+class History {
+
+};
+
+
+
+#endif //HISTORY_H
